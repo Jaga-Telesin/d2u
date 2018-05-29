6c1c946db44d85405ab7154dae034236553625ff
Jaga-Telesin <39468347+Jaga-Telesin@users.noreply.github.com>
2018-05-29 15:38:04 -0600

    Update variables.py
    Tweaked step_delay to slightly more forgiving value to accomodate slower(er) servers.

--- deluge_scripts/variables.py
+++ deluge_scripts/variables.py
@@ -4,7 +4,7 @@
 deluge_port  = 21000                       # Server daemon port (integer)
 min_ratio    = 1.0                         # Don't move torrents until they reach or exceed a ratio of X
 output       = False                       # Set to 'False' for cron jobs. Set to 'True' to see output in a shell.
-step_delay   = 3                           # If your server has problems with the script speed, increase this value
+step_delay   = 5                           # If your server has problems with the script speed, increase this value
 home_dir     = '/home/user/'               # Replace with your home directory. Should be similar to '/home/user/'
 auto_dir     = 'downloads/d2u/'            # Location of d2u folder structure (under your home directory)
 trash_dir    = home_dir+auto_dir+'trash/'  # Duplicate torrents end up here
