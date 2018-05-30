37150c98f58a8cc6c14f83fc64faa371040cb431
Jaga-Telesin <39468347+Jaga-Telesin@users.noreply.github.com>
2018-05-29 21:22:02 -0600

    v1.1

--- deluge_scripts/variables.py
+++ deluge_scripts/variables.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
-# Created by u/JagaTelesin (reddit)
+# Created by u/JagaTelesin (reddit) - John B.
 
-deluge_port  = 21000                       # Server daemon port (integer)
 min_ratio    = 1.0                         # Don't move torrents until they reach or exceed a ratio of X
+                                           # NOTE: min_ratio is able to be passed to the script as a command line parameter
+                                           #   ex:  python d2u.py 0.8
+deluge_port  = 21000                       # Server daemon port (integer)
 output       = False                       # Set to 'False' for cron jobs. Set to 'True' to see output in a shell.
-step_delay   = 5                           # If your server has problems with the script speed, increase this value
+step_delay   = 1                           # If your server has problems with the script speed, increase this value
 home_dir     = '/home/user/'               # Replace with your home directory. Should be similar to '/home/user/'
 auto_dir     = 'downloads/d2u/'            # Location of d2u folder structure (under your home directory)
 trash_dir    = home_dir+auto_dir+'trash/'  # Duplicate torrents end up here
